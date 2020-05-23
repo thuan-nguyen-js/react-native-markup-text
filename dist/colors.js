43aa982eacbf895a5aaabd071c832ab58de0e07b
Thuan <thuannbd@logivan.com>
2020-05-24 01:25:30 +0700

    update build

--- dist/colors.js
+++ dist/colors.js
@@ -1,1 +1,1 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;var Colors={tomato:'rgb(229, 56, 56)',grey:'#cccccc'};var _default=Colors;exports.default=_default;
+Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;var Colors={tomato:'rgb(229, 56, 56)',warning:'#ffae42',grey:'#cccccc'};var _default=Colors;exports.default=_default;
